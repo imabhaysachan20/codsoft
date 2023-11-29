6de28235f8dddcd4f04192464950247de4766c31
imabhaysachan20 <150459720+imabhaysachan20@users.noreply.github.com>
2023-11-29 16:26:21 +0530

    Add files via upload

---
+++ Portfolio Website_files/main.js
@@ -0,0 +1,7 @@
+var typed  =new Typed('.text',{
+    strings:['Frontend Developer','Java Programmer','2nd Year Engineering Student'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
